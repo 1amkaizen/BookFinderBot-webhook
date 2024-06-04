3f6229279727a3211c7337d249f976ef28d49a3a
1amkaizen <1amkaiz3n@gmail.com>
2024-06-04 17:02:37 +0700

    update

--- user/datauser.go
+++ user/datauser.go
@@ -34,7 +34,7 @@ func SaveUserDataToHTML(users []UserData, filename string) error {
 	<head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>AdminLTE 3 | DataTables</title>
+	<title>BookFinderBot | DataTables</title>
 	
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 	
